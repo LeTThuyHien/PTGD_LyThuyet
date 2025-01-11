e1985725370bad7ba017159fd543fa39d3be38ee
LeTThuyHien <thuyhienle1664@gmail.com>
2025-01-11 10:04:31 +0700

    XMLHttpRequest RESTful GET test 2

--- js/XMLHttpRequest RESTful GET.js
+++ js/XMLHttpRequest RESTful GET.js
@@ -1,13 +1,14 @@
+console.log("Starting XMLHttpRequest RESTful GET");
 // Sử dụng API miễn phí thay thế
 var url = "https://jsonplaceholder.typicode.com/users";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
+var xhr = new XMLHttpRequest()
+xhr.open('GET', url + '/1', true)
 xhr.onload = function () {
     var users = JSON.parse(xhr.responseText);
-    if (xhr.readyState == 4 && xhr.status == 200) {
+    if (xhr.readyState == 4 && xhr.status == "200") {
         console.table(users);
     } else {
-        console.error("Error:", xhr.responseText);
+        console.error(users);
     }
-};
+}
 xhr.send(null);
